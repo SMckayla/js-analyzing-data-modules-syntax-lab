a58e197855fcae7ea05c845c805ce9bcf9cfc589
Sascha Mckayla <mckaylasascha@gmail.com>
2026-04-16 22:45:21 +0300

    fix ignore rules

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
+//adding dates
+require("datejs")
+
 //combining users
 function combineUsers(...args){
   let combinedObject = {users: []}
 };
 
-//adding dates
-require("datejs")
-
 //creating the loop for the above functions
-args.array.forEach(array=> {combinedObject.users.push(...arr);
+args.forEach(arr=> {combinedObject.users.push(...arr);
   });
 
   //adding the date format
-  combinedObject.merge_date= newDate()toString('m/d/yyyy');
-  
+combinedObject.merge_date= newDate()toString() M/d/yyyy;
 
-
-
-
-
+return combinedObject;
 
 
 module.exports = {
